2757ccddc172b2b531620dd4178d164c3ba9c9bb
ibad887 <135799717+ibad887@users.noreply.github.com>
2023-11-01 14:37:58 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 
 global.creator = 'manusia'// yourname
 global.apikey = ["rawrkey"]
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://sayaadalah887:adalah887@cluster0.uof9cob.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "sayaadalah887@gmail.com" //email
+global.email_password = "manusia" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
